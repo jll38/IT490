8976713a202b3101adeefd640e2c711d9246a4f0
Julian Lechner <jll38@njit.edu>
2024-04-12 14:52:56 -0400

    dev environment ssl check

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
 import uvicorn
 import ssl
-
+import os
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 ssl_context.load_cert_chain('./cert.pem', keyfile='./key.pem')
 
 if __name__ == '__main__':
-    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True,
-                ssl_keyfile='./key.pem', ssl_certfile='./cert.pem')
+    if os.getenv('ENVIRONMENT') == 'production':
+        uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True,
+                    ssl_keyfile='./key.pem', ssl_certfile='./cert.pem')
+    else:
+        uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True,)
