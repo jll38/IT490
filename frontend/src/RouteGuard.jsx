2b39bdebe70ca886ac5540183423f1be58305d3f
ak256 <68403972+ak256-ak@users.noreply.github.com>
2024-05-05 20:43:59 -0400

    Update RouteGuard.jsx

--- frontend/src/RouteGuard.jsx
+++ frontend/src/RouteGuard.jsx
@@ -1,11 +1,10 @@
 import { Navigate } from "react-router-dom";
 import { User } from "./lib/token";
 export function RouteGuard({ children }) {
-  console.log("User:");
-  console.log(User);
-  console.log("---------")
-  if (!User) {
-    // Redirect them to home page or elsewhere if they do not meet the condition
+console.log("User is authenticated:", User ? true : false);
+
+  if (!User || !User.isLoggedIn) { 
+   n
     return <Navigate to="/" replace />;
   }
 
